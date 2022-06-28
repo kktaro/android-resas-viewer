b2edc15d0ae64d2be068a1e09ceb9baf06fda088
コタロー <kk.nnkktr@gmail.com>
2022-06-28 16:39:24 +0900

    アプリ名を設定

---
+++ app/src/main/java/com/example/androidresasviewer/settings/settings.kt
@@ -0,0 +1,7 @@
+package com.example.androidresasviewer.settings
+
+class Settings {
+    companion object {
+        const val APP_NAME = "Population Viewer"
+    }
+}
--- app/src/main/java/com/example/androidresasviewer/view/MainView.kt
+++ app/src/main/java/com/example/androidresasviewer/view/MainView.kt
@@ -2,27 +2,17 @@ package com.example.androidresasviewer.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.androidresasviewer.model.repository.ResasRepositoryImpl
-import com.example.androidresasviewer.model.repository.ResasRepositoryModule
+import com.example.androidresasviewer.settings.Settings.Companion.APP_NAME
 import com.example.androidresasviewer.view.component.molecule.LabelledCheckbox
 import com.example.androidresasviewer.view.component.organism.PopulationGraph
 import com.example.androidresasviewer.viewmodel.MainViewModel
-import java.util.*
 
 @ExperimentalMaterial3Api
 @Composable
@@ -33,7 +23,7 @@ fun MainView(viewModel: MainViewModel) {
             .fillMaxWidth(),
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text("APP") },
+                title = { Text(APP_NAME) },
             )
         },
     ) {
